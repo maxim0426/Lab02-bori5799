aab2482255c55320f71334238b32ddf6ac17d89a
bori5799 <bori5799@stthomas.edu>
2019-09-24 23:20:37 -0500

    second commit

---
+++ main.tf
@@ -0,0 +1,57 @@
+provider "google" {
+  project = "lab-2-project-254002"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
+
+resource "google_compute_instance" "svc-1" {
+  name         = "bori5799-testvm1"
+  machine_type = "f1-micro"
+  zone         = "us-central1-c"
+
+  metadata = {
+    ssh-keys = "INSERT_USERNAME:${file("~/.ssh/google_compute_engine.pub")}"
+    #startup_script = "echo hi > /test.txt" # doesn't work
+   }
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral IP
+    }
+  }
+
+  provisioner "remote-exec" {
+    connection {
+      host        = "${google_compute_instance.svc-1.network_interface.0.access_config.0.nat_ip}"
+      user        = "bori5799"
+      type        = "ssh"
+      private_key = "${file("~/.ssh/google_compute_engine")}"
+      }
+    inline = [
+      "mkdir -p ~/svc-01/html",
+    ]
+  }
+
+  provisioner "file" {
+    source      = "svc-01/"
+    destination = "~/svc-01"
+    connection {
+      host        = "${google_compute_instance.svc-1.network_interface.0.access_config.0.nat_ip}"
+      user        = "bori5799"
+      type        = "ssh"
+      private_key = "${file("~/.ssh/google_compute_engine")}"
+    }
+  }
+
+  provisioner "remote-exec" {
+    connection {
+      host        = "${google_compute_instance.svc-1.network_interface.0.access_config.0.nat_ip}"
+      user        = "bori579
